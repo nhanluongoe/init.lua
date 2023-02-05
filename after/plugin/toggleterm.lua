3d06d52d707859fc8d85280c4a73866747fba8c8
nhanluongoe <nhanlt18@gmail.com>
2023-02-06 01:31:38 +0700

    remap toggleterm for lazygit

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -21,4 +21,4 @@ function _lazygit_toggle()
 	lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gs", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
