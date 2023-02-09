a4b7429e6a5c294a3c9c985a4d6b7cee6606ba80
nhanluongoe <nhanlt18@gmail.com>
2023-02-09 13:38:13 +0700

    fix: disable esc keymap in toggle term which is also used for lazygit

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -32,7 +32,7 @@ require("toggleterm").setup({
 
 function _G.set_terminal_keymaps()
 	local opts = { buffer = 0 }
-	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+	-- vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
 	vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
 	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
 	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
