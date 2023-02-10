25f7e1c1fc7efb4c7d00e69609a821b4b3b40740
nhanluongoe <nhanlt18@gmail.com>
2023-02-10 09:21:33 +0700

    comment unused code

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,12 +1,12 @@
-function ColorMyPencils(color)
-	color = color or "onedark"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-ColorMyPencils()
+-- function ColorMyPencils(color)
+-- 	color = color or "onedark"
+-- 	vim.cmd.colorscheme(color)
+--
+-- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- end
+--
+-- ColorMyPencils()
 
 function HideSplitBorder()
 	vim.cmd("highlight VertSplit guibg=bg guifg=#1E232A")
