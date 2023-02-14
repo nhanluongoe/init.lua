480c53eaff643bcc1f770545aff1d46d905a3a73
nhanluongoe <nhanlt18@gmail.com>
2023-02-14 23:44:05 +0700

    disable format on save

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,34 +1,34 @@
 -- import mason plugin safely
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-  return
+	return
 end
 
 -- import mason-null-ls plugin safely
 local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
 if not mason_null_ls_status then
-  return
+	return
 end
 
 -- enable mason
 mason.setup()
 
 mason_null_ls.setup({
-  -- list of formatters & linters for mason to install
-  ensure_installed = {
-    "prettier", -- ts/js formatter
-    "stylua", -- lua formatter
-    "eslint_d", -- ts/js linter
-    "yamlfmt", -- yaml formatter
-  },
-  -- auto-install configured formatters & linters (with null-ls)
-  automatic_installation = true,
+	-- list of formatters & linters for mason to install
+	ensure_installed = {
+		"prettier", -- ts/js formatter
+		"stylua", -- lua formatter
+		"eslint_d", -- ts/js linter
+		"yamlfmt", -- yaml formatter
+	},
+	-- auto-install configured formatters & linters (with null-ls)
+	automatic_installation = true,
 })
 
 -- import null-ls plugin safely
 local setup, null_ls = pcall(require, "null-ls")
 if not setup then
-  return
+	return
 end
 
 -- for conciseness
@@ -40,37 +40,37 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- configure null_ls
 null_ls.setup({
-  -- setup formatters & linters
-  sources = {
-    --  to disable file types use
-    --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-    formatting.prettier, -- js/ts formatter
-    formatting.stylua, -- lua formatter
-    formatting.yamlfmt,
-    diagnostics.eslint_d.with({ -- js/ts linter
-      -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
-      condition = function(utils)
-        return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
-      end,
-    }),
-  },
-  -- configure format on save
-  on_attach = function(current_client, bufnr)
-    if current_client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({
-            filter = function(client)
-              --  only use null-ls for formatting instead of lsp server
-              return client.name == "null-ls"
-            end,
-            bufnr = bufnr,
-          })
-        end,
-      })
-    end
-  end,
+	-- setup formatters & linters
+	sources = {
+		--  to disable file types use
+		--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
+		formatting.prettier, -- js/ts formatter
+		formatting.stylua, -- lua formatter
+		formatting.yamlfmt,
+		diagnostics.eslint_d.with({ -- js/ts linter
+			-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
+			condition = function(utils)
+				return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
+			end,
+		}),
+	},
+	-- configure format on save
+	-- on_attach = function(current_client, bufnr)
+	--   if current_client.supports_method("textDocument/formatting") then
+	--     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+	--     vim.api.nvim_create_autocmd("BufWritePre", {
+	--       group = augroup,
+	--       buffer = bufnr,
+	--       callback = function()
+	--         vim.lsp.buf.format({
+	--           filter = function(client)
+	--             --  only use null-ls for formatting instead of lsp server
+	--             return client.name == "null-ls"
+	--           end,
+	--           bufnr = bufnr,
+	--         })
+	--       end,
+	--     })
+	--   end
+	-- end,
 })
