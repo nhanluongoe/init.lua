17391b62de19ba8689e43d839edc204641ca916d
nhanluongoe <nhanlt18@gmail.com>
2023-02-24 19:31:17 +0700

    safely import telescope builtin

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,10 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+local status, telescope_builtin = pcall(require, 'telescope.builtin');
+if not status then
+  return
+end
+
+vim.keymap.set('n', '<leader>pf', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', telescope_builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+	telescope_builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
