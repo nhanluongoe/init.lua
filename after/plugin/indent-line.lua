02a0bb3f2c1e4837ebb1894941dcd61c4b41b20d
nhanluongoe <nhanlt18@gmail.com>
2023-03-07 15:27:10 +0700

    restyle indent line

--- after/plugin/indent-line.lua
+++ after/plugin/indent-line.lua
@@ -5,9 +5,9 @@ end
 
 indent_line.setup({
 	show_end_of_line = true,
-	space_char_blankline = " ",
+	-- space_char_blankline = " ",
 })
 
 vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:↴")
+-- vim.opt.listchars:append("space:⋅")
+-- vim.opt.listchars:append("eol:↴")
