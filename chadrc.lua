825020d4749d203e8981b4f280be36d695299a3a
nhanluongoe <nhanlt18@gmail.com>
2023-06-04 09:39:21 +0700

    chnage theme to gruvchad

--- chadrc.lua
+++ chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "github_dark",
-  theme_toggle = { "github_dark", "one_light" },
+  theme = "gruvchad",
+  theme_toggle = { "gruvchad", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
