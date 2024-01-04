d6cf4b2801d2359e4ec567d1d19960fb21c15021
nhanluongoe <nhanlt18@gmail.com>
2024-01-04 20:52:04 +0700

    chore: use eslint_d instead and add angular language server

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -44,9 +44,7 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup {
   sources = {
     null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.diagnostics.eslint_d.with {
-      diagnostics_format = "[eslint] #{m}\n(#{c})",
-    },
+    null_ls.builtins.diagnostics.eslint_d,
   },
   -- on_attach = function(client, bufnr)
   --   if client.supports_method "textDocument/formatting" then
